f301e1e6660f72843c9adaf45b0c6873d1951484
Marc Hauschildt <marc.hauschildt@gmail.com>
2024-02-26 15:24:40 -0600

    mon feb 26

---
+++ CodeDemos/src/main/java/edu/kirkwood/pair_programming/Assignment9.java
@@ -0,0 +1,9 @@
+package edu.kirkwood.pair_programming;
+
+import java.util.Scanner;
+
+public class Assignment9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ CodeDemos/src/test/java/edu/kirkwood/pair_programming/Assignment9Test.java
@@ -0,0 +1,7 @@
+package edu.kirkwood.pair_programming;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Assignment9Test {
+
+}
