9752566572f9c7e32c52da1757bef20aa9f9328f
Marc Hauschildt <marc.hauschildt@gmail.com>
2024-02-28 10:40:20 -0600

    wed feb 28

--- CodeDemos/src/main/java/edu/kirkwood/pair_programming/Assignment9.java
+++ CodeDemos/src/main/java/edu/kirkwood/pair_programming/Assignment9.java
@@ -1,9 +1,28 @@
 package edu.kirkwood.pair_programming;
 
+import edu.kirkwood.shared.Helpers;
+
 import java.util.Scanner;
 
 public class Assignment9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // Demo Task
+        System.out.print("Enter a number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter another number: ");
+        double num2 = scanner.nextDouble();
+        double sum = getSum(num1, num2);
+        System.out.printf("%s + %s = %s\n", num1, num2, Helpers.round(sum, 1));
     }
+
+    /**
+     * @param num1 A double
+     * @param num2 A double
+     * @return The sum of num1 and num2
+     */
+    public static double getSum(double num1, double num2) {
+        return num1 + num2;
+    }
+
 }
--- CodeDemos/src/test/java/edu/kirkwood/pair_programming/Assignment9Test.java
+++ CodeDemos/src/test/java/edu/kirkwood/pair_programming/Assignment9Test.java
@@ -1,7 +1,13 @@
 package edu.kirkwood.pair_programming;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Assignment9Test {
-
+    @Test
+    void getSum() {
+        assertEquals(3, Assignment9.getSum(1, 2));
+        assertEquals(3.3, Assignment9.getSum(1.1, 2.2), 1);
+    }
 }
